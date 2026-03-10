a60f499d342584f5c18a537999e5f0556dc1ec84
falahadeel-ctrl <falahadeel@gmail.com>
2026-03-10 16:25:02 -0400

    rfbvfs

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { Routes, Route } from 'react-router-dom'
+import Navbar from './components/Navbar'
 import Dashboard from './pages/Dashboard'
 import Logs from './pages/Logs'
 import Notes from './pages/Notes'
@@ -6,12 +7,15 @@ import Settings from './pages/Settings'
 
 function App() {
   return (
+    <>
+    <Navbar />
     <Routes>
       <Route path='/' element={<Dashboard />} />
       <Route path='/logs' element={<Logs />} />
       <Route path='/notes' element={<Notes />} />
       <Route path='/settings' element={<Settings />} />
     </Routes>
+    </>
   )
 }
 
