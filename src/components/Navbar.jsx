a9d25818d9d0100e810b798d0e1d5e76bc96ff26
falahadeel-ctrl <falahadeel@gmail.com>
2026-03-10 16:20:53 -0400

    brberw

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,12 @@ import {Link} from 'react-router-dom'
 function Navbar(){
     return (
         <nav>
-            Link to='/'
+           < Link to='/'>Dashboard</Link>
+           <Link to='/logs'>Logs</Link>
+           <Link to='/notes'>Notes</Link>
+           <Link to='/settings'>settings</Link>
         </nav>
     )
 }
+
+export default Navbar
